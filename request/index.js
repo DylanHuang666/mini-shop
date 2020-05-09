41a86f0f5ed5d8e7d1115c727c89dad4d8332c0c
Dylan <huanglinjay123@outlook.com>
2020-05-10 07:26:39 +0800

    完成商品列表页，封装Tabs标签页组件。

--- request/index.js
+++ request/index.js
@@ -1,4 +1,12 @@
+// 同时发送异步代码的次数
+let ajaxTimes = 0;
 export const request = (params)=>{
+  ajaxTimes++;
+  // 显示加载中 效果
+  wx.showLoading({
+    title: "加载中",
+    mask: true
+  });
   const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
   return new Promise((resolve,reject)=>{
     wx.request({
@@ -9,6 +17,13 @@ export const request = (params)=>{
       },
       fail(err){
         reject(err);
+      },
+      complete(){
+        ajaxTimes--;
+        if (ajaxTimes === 0) {
+          //  关闭加载中的图标
+          wx.hideLoading();
+        }
       }
     })
   });
